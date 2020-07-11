3d6da0598252c42df53bcb0af621bccd268462f1
romario-auth <romario.auth@gmail.com>
2020-07-11 15:57:37 -0300

    Finish

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Election
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path:");
+            string path = Console.ReadLine();
+
+            Dictionary<string, int> election = new Dictionary<string, int>();
+
+            try{
+                using (StreamReader file = File.OpenText(path))
+                {
+                    while(!file.EndOfStream)
+                    {
+                        string[] line = file.ReadLine().Split(",");
+                        string name = line[0].ToString();
+                        int wishes = int.Parse(line[1]);
+
+                        if(election.ContainsKey(name))
+                        {
+                            election[name] += wishes;
+                        } else
+                        {
+                            election[name] = wishes;
+                        }
+                    }
+                    
+                }
+
+                foreach(KeyValuePair<string, int> item in election)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+
+        }
+    }
+}
